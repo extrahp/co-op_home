232130c9b2aa825255eb50e3198a6768b8a6519b
extrahp <extrahitpoints@gmail.com>
2016-06-02 00:39:41 -0400

    added white mountain

---
+++ Assets/clickNextLevel.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clickNextLevel : MonoBehaviour {
+	public GameObject triggerObject;
+	public string levelname;
+	bool enableExit = false;
+	bool displaytext;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject == triggerObject) {
+			displaytext = true;
+			enableExit = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject == triggerObject) {
+			displaytext = false;
+			enableExit = false;
+		}
+	}
+
+	void OnMouseDown() {
+		if (enableExit) {
+			Application.LoadLevel ("new_level4_white_mountain");
+		}
+	}
+
+	void OnGUI() {
+		if (displaytext) {
+			GUI.Label (new Rect (Screen.width/2-100, Screen.height/2, 200, 20), "Click to enter the train.");
+		}
+	}
+}
